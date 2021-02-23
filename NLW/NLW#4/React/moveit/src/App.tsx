92617ae3db418587201e5ce877befcb8ed1ec2b4
alberto <albertojr11@hotmail.com>
2021-02-23 14:51:03 -0300

    Updates from my Studies on Rocketseat NLW#4

---
+++ NLW/NLW#4/React/moveit/src/App.tsx
@@ -0,0 +1,14 @@
+import { ExperienceBar } from './components/ExperienceBar';
+
+
+import './style/global.css';
+
+function App() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default App;
