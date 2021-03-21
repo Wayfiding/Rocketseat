dfdf5d97c42c46298d2ba27e431df3b17125781c
alberto <albertojr11@hotmail.com>
2021-03-21 13:21:54 -0300

    Authenticate implemented, controllers,middlewares and Tokens implemented too. In a way that everything use Token to confirm the password and user data.

--- serie-node/src/controllers/authController.js
+++ serie-node/src/controllers/authController.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const bcrypt = require('bcryptjs')
 
+const jwt = require('jsonwebtoken')
+const authConfig = require('../config/auth')
+
 const User = require('../models/user');
 
 const router = express.Router();
+
+function generateToken( params={} ){
+    return jwt.sign(params, authConfig.secret, {
+        expiresIn:86400,
+    })
+
+}
 
 
 router.post('/register', async (req,res) => {
@@ -17,7 +27,11 @@ router.post('/register', async (req,res) => {
 
         user.password = undefined;
 
-        return res.send({ user });
+        return res.send({ 
+            user,
+            token: generateToken({ id: user.id }) 
+        
+         });
 
     } catch(err) {
         return res.status(400).send({ error: 'Registration failed'});
@@ -37,7 +51,13 @@ router.post('/authenticate', async (req,res) => {
     if(!await bcrypt.compare(password, user.password))
         return  res.status(400).send({ error: 'Invalid Password'});
 
-    res.send({ user });
+    user.password = undefined;
+
+   
+
+    res.send({ 
+        user,
+        token:generateToken({id: user.id }) });
 })
 
 
