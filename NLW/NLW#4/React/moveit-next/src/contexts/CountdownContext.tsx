b1c7731098c968e34ee4be8b93a4a8f479754e1d
alberto <albertojr11@hotmail.com>
2021-03-21 01:25:48 -0300

    Some updates on READ.me Projects

--- NLW/NLW#4/React/moveit-next/src/contexts/CountdownContext.tsx
+++ NLW/NLW#4/React/moveit-next/src/contexts/CountdownContext.tsx
@@ -24,7 +24,7 @@ export function CountdownProvider ({ children } ){
 
 
 
-    const [time,setTime] = useState( 0.1 * 60);
+    const [time,setTime] = useState( 25 * 60);
     const [isActive,setIsActive] = useState(false);
     const [hasFinished, setHasFinished] = useState(false);
 
