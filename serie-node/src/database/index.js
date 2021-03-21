548478108eeb549413093fce90696cc69c95b51c
alberto <albertojr11@hotmail.com>
2021-03-20 23:52:39 -0300

    API node+express+mongo, data structure and register

---
+++ serie-node/src/database/index.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+
+
+mongoose.connect('mongodb://localhost/noderest', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+mongoose.Promise = global.Promise;
+
+
+
+module.exports = mongoose;
